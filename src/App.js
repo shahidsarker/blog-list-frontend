058604b85641a4395b085218b155b218d5c3abc6
Shahid <shahid.sarker@gmail.com>
2020-05-12 23:11:31 -0400

    conditional rendering of loginform and bloglist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
+import loginService from "./services/login";
+import "./App.css";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form>//...</form>
-      </div>
-    );
-  }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
 
-  return (
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log("Wrong credentials");
+    }
+  };
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+
+  const blogList = () => (
     <div>
       <h2>blogs</h2>
       {blogs.map((blog) => (
@@ -26,6 +62,8 @@ const App = () => {
       ))}
     </div>
   );
+
+  return user === null ? loginForm() : blogList();
 };
 
 export default App;
