032fcb4e401f655baf261f8da42c3526566dc059
Shahid <shahid.sarker@gmail.com>
2020-05-12 22:56:49 -0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form>//...</form>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
