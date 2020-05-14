1b11cb2b6b67f872de30cef264b99a5bef0ea068
Shahid <shahid.sarker@gmail.com>
2020-05-13 23:12:58 -0400

    clean up logged in page

--- src/App.js
+++ src/App.js
@@ -132,10 +132,11 @@ const App = () => {
   const blogList = () => (
     <div>
       <h2>blogs</h2>
-      {blogForm()}
+      <p>
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
+      </p>
 
-      <p>{user.name} logged in</p>
-      <button onClick={handleLogout}>logout</button>
+      {blogForm()}
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
