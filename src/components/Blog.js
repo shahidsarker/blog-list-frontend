a7aa7800d8e6b11a9daf2e3f05133c8eae08e55e
Shahid <shahid.sarker@gmail.com>
2020-05-17 22:13:27 -0400

    Blog has details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [expanded, setExpanded] = useState(Math.random() < 0.5);
+
+  const handleToggle = (e) => {
+    e.preventDefault();
+    setExpanded(!expanded);
+  };
+
+  const details = () => {
+    if (expanded) {
+      return (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button>like</button>
+          </div>
+          {blog.user && <div>{blog.user.name}</div>}
+        </>
+      );
+    }
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={handleToggle}>{expanded ? "hide" : "view"}</button>
+        {details()}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
