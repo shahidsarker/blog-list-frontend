9a41ce2377e9e5d834058a3a8fd4e4129d0d987a
Shahid <shahid.sarker@gmail.com>
2020-06-17 02:02:57 -0400

    test that url and likes are displayed upon click

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -19,7 +19,7 @@ test('renders blog title and author without url or likes', () => {
     />
   )
   const blogDiv = component.container.querySelector('.blog')
-  const blogTitle = component.container.querySelector('.blog-title')
+
   const blogUrl = component.container.querySelector('.blog-url')
   const blogLikes = component.container.querySelector('.blog-likes')
 
@@ -27,4 +27,31 @@ test('renders blog title and author without url or likes', () => {
   expect(blogDiv).toHaveTextContent(blog.author)
   expect(blogUrl).toBe(null)
   expect(blogLikes).toBe(null)
+})
+
+test('blog url and likes shown when button is clicked', () => {
+  const blog = {
+    title: 'Hello world',
+    author: 'John Doe',
+    url: 'http://example.com',
+    likes: 5,
+  }
+
+  const component = render(
+    <Blog
+      blog={blog}
+      updateBlog={() => console.log('update blog')}
+      deleteBlog={() => console.log('delete blog')}
+    />
+  )
+
+  const blogToggle = component.container.querySelector('.blog-toggle')
+  fireEvent.click(blogToggle)
+
+  const blogUrl = component.container.querySelector('.blog-url')
+  const blogLikes = component.container.querySelector('.blog-likes')
+
+  expect(blogUrl).toHaveTextContent(blog.url)
+
+  expect(blogLikes).toHaveTextContent(String(blog.likes))
 })
