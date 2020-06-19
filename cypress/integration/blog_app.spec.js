223094d558b08e6621dead7a685557d99f5d6471
Shahid <shahid.sarker@gmail.com>
2020-06-19 02:03:13 -0400

    set up end to end testing

---
+++ cypress/integration/blog_app.spec.js
@@ -0,0 +1,43 @@
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function () {
+    cy.contains('gotta fix this')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.contains('gotta fix this')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.contains('gotta fix this')
+    })
+  })
+
+  describe.only('when logged in', function () {
+    beforeEach(function () {
+      cy.contains('gotta fix this')
+    })
+
+    it('A blog can be created', function () {
+      cy.contains('gotta fix this')
+    })
+
+    it('A user can like a blog', function () {
+      cy.contains('gotta fix this')
+    })
+
+    describe('Deleting a blog', function () {
+      it('user who created can', function () {
+        cy.contains('gotta fix this')
+      })
+      it('user who did not create cannot', function () {
+        cy.contains('gotta fix this')
+      })
+    })
+  })
+})
