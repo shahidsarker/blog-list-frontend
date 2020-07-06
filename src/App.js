35e2323128d0ac21d6c7b77fa92711a9bc6ba3a3
Shahid <shahid.sarker@gmail.com>
2020-07-06 18:46:49 -0400

    extract BlogDisplay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { useEffect } from 'react'
-import Blog from './components/Blog'
 import Notification from './components/Notification'
-import Toggleable from './components/Toggleable'
-import BlogForm from './components/BlogForm'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import {
-  initializeBlogs,
-  createBlog,
-  likeBlog,
-  removeBlog,
-} from './reducers/blogReducer'
-import { setNotification } from './reducers/notificationReducer'
+import { initializeBlogs } from './reducers/blogReducer'
 import { initializeUser, logoutUser } from './reducers/userReducer'
 import Login from './components/Login'
+import BlogDisplay from './components/BlogDisplay'
 
 const App = () => {
   const dispatch = useDispatch()
-  const blogs = useSelector((state) => state.blogs)
   const notification = useSelector((state) => state.notification)
   const user = useSelector((state) => state.user)
 
@@ -31,75 +22,20 @@ const App = () => {
     dispatch(logoutUser())
   }
 
-  const handleCreateBlog = async (blogObject) => {
-    try {
-      blogFormRef.current.toggleVisibility()
-      dispatch(createBlog(blogObject))
-
-      dispatch(
-        setNotification({
-          type: 'success',
-          text: `new blog ${blogObject.title} by ${blogObject.author} added`,
-        })
-      )
-    } catch (exception) {
-      console.log('new blog unsuccessful')
-    }
-    // send three fields as post request via create service
-    // update state
-    // clear old values from state
-    // use response to update blogs state by concat
-  }
-
-  const blogFormRef = React.createRef()
-
-  const blogForm = () => (
-    <Toggleable buttonLabel="new blog" ref={blogFormRef}>
-      <BlogForm createBlog={handleCreateBlog} />
-    </Toggleable>
-  )
-
-  const handleLikeBlog = (id, updatedObj) => {
-    try {
-      dispatch(likeBlog(id, updatedObj))
-    } catch (exception) {
-      console.log('blog update unsuccessful')
-    }
-  }
-
-  const handleDeleteBlog = async (id) => {
-    try {
-      dispatch(removeBlog(id))
-    } catch (exception) {
-      console.log('error deleting')
-    }
-  }
-
-  const blogDisplay = () => (
-    <>
-      <p>
-        {user.name} logged in <button onClick={handleLogout}>logout</button>
-      </p>
-
-      {blogForm()}
-      <div id="blog-list">
-        {blogs.map((blog) => (
-          <Blog
-            key={blog.id}
-            blog={blog}
-            updateBlog={handleLikeBlog}
-            deleteBlog={handleDeleteBlog}
-          />
-        ))}
-      </div>
-    </>
-  )
-
   return (
     <div>
       <h2>blogs</h2>
       <Notification message={notification} />
-      {user === null ? <Login /> : blogDisplay()}
+      {user === null ? (
+        <Login />
+      ) : (
+        <>
+          <p>
+            {user.name} logged in <button onClick={handleLogout}>logout</button>
+          </p>
+          <BlogDisplay />
+        </>
+      )}
     </div>
   )
 }
---
+++ src/components/BlogDisplay.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Toggleable from './Toggleable'
+import BlogForm from './BlogForm'
+import Blog from './Blog'
+import { useDispatch, useSelector } from 'react-redux'
+import { createBlog, likeBlog, removeBlog } from '../reducers/blogReducer'
+import { setNotification } from '../reducers/notificationReducer'
+
+const BlogDisplay = (props) => {
+  const dispatch = useDispatch()
+  const blogs = useSelector((state) => state.blogs)
+
+  const handleCreateBlog = async (blogObject) => {
+    try {
+      blogFormRef.current.toggleVisibility()
+      dispatch(createBlog(blogObject))
+
+      dispatch(
+        setNotification({
+          type: 'success',
+          text: `new blog ${blogObject.title} by ${blogObject.author} added`,
+        })
+      )
+    } catch (exception) {
+      console.log('new blog unsuccessful')
+    }
+  }
+
+  const blogFormRef = React.createRef()
+
+  const blogForm = () => (
+    <Toggleable buttonLabel="new blog" ref={blogFormRef}>
+      <BlogForm createBlog={handleCreateBlog} />
+    </Toggleable>
+  )
+
+  const handleLikeBlog = (id, updatedObj) => {
+    try {
+      dispatch(likeBlog(id, updatedObj))
+    } catch (exception) {
+      console.log('blog update unsuccessful')
+    }
+  }
+
+  const handleDeleteBlog = async (id) => {
+    try {
+      dispatch(removeBlog(id))
+    } catch (exception) {
+      console.log('error deleting')
+    }
+  }
+
+  return (
+    <>
+      {blogForm()}
+      <div id="blog-list">
+        {blogs.map((blog) => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            updateBlog={handleLikeBlog}
+            deleteBlog={handleDeleteBlog}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default BlogDisplay
