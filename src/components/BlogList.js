4523f55be11eaee2cad6a4ca6b4c955fdd3ef85d
Shahid <shahid.sarker@gmail.com>
2020-07-16 23:11:28 -0400

    blog list table material-ui

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  Paper,
+  TableHead,
+  TableRow,
+} from '@material-ui/core'
 
 const BlogList = ({ blogs }) => {
   if (!blogs.length) {
@@ -7,13 +16,30 @@ const BlogList = ({ blogs }) => {
   }
   return (
     <div id="blog-list">
-      <ul>
-        {blogs.map((blog) => (
-          <li key={blog.id}>
-            <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Likes</TableCell>
+              <TableCell align="right">Added by</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {blogs.map((blog) => (
+              <TableRow key={blog.id}>
+                <TableCell>
+                  <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+                </TableCell>
+                <TableCell align="right">{blog.likes}</TableCell>
+                <TableCell align="right">
+                  {blog.user ? blog.user.name : ''}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
